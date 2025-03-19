a9e61803a94c75fcea4dc07c954d9f2fbadbc66e
Joshua-Velozo <joshuavelozousa@gmail.com>
2025-03-19 15:24:46 -0400

    pokemon.py version 1.1

--- pokemon.py
+++ pokemon.py
@@ -59,19 +59,18 @@ bulbasaurArt = """
 
 
 
-class pokemon :
-    def __init__(self, name, types, moves, EVs, pics, health ='=============================='):
+import time
+
+class Pokemon:
+    def __init__(self, name, types, moves, EVs, pics, health='=============================='):
         self.name = name
         self.types = types
-        self.moves = [(move, move_type) for move, move_type in moves] 
+        self.moves = [(move, move_type) for move, move_type in moves]
         self.defense = EVs['DEFENSE']
         self.attack = EVs['ATTACK']
         self.pics = pics
         self.healthBar = 30
         self.health = health
-        
-
-
 
     def fight(self, pokemon2):
         print("-----POKEMON BATTLE SIMULATOR-----")
@@ -79,44 +78,38 @@ class pokemon :
         print(f"\nTYPE/ {self.types}")
         print(f"\nATTACK/ {self.attack}")
         print(f"\nDEFENSE/ {self.defense}")
-    
-    
 
         print(f"\n{pokemon2.name}")
         print(f"\nTYPE/ {pokemon2.types}")
         print(f"\nATTACK/ {pokemon2.attack}")
         print(f"\nDEFENSE/ {pokemon2.defense}")
-    
-    
-    
 
-        element = ['Grass', 'Fire', 'Water']
+        elements = ['Grass', 'Fire', 'Water']
 
         def calculate_effectiveness(move_type, defender_type):
-            for i, k in enumerate(element):
-                if move_type == k:
-                    if defender_type == element[(i + 1) % 3]:
-                        return 2  # Super effective
-                    elif defender_type == element[(i + 2) % 3]:
-                        return 0.5  # Not very effective
+            if move_type in elements and defender_type in elements:
+                move_index = elements.index(move_type)
+                defender_index = elements.index(defender_type)
+                if (move_index - defender_index) % 3 == 1:  # Strong against next in cycle
+                    return 2  # Super effective
+                elif (move_index - defender_index) % 3 == 2:  # Weak against previous in cycle
+                    return 0.5  # Not very effective
             return 1  # Neutral
 
-
-
-        while (pokemon2.healthBar > 0) and (self.healthBar > 0):
-
+        while pokemon2.healthBar > 0 and self.healthBar > 0:
             print(f"\n{self.name}\t\tHLTH\t{self.health}")
             print(self.pics)
             print(f"\n{pokemon2.name}\t\tHLTH\t{pokemon2.health}")
             print(pokemon2.pics)
 
             print(f'GO {self.name}!')
-            for i, x in enumerate(self.moves):
-                print(f'{i + 1}.', x)
-            index = int(input('PICK A MOVE'))
-            print(f'{self.name} used {self.moves[index - 1]}!')
+            for i, (move_name, move_type) in enumerate(self.moves):
+                print(f'{i + 1}. {move_name}')
+            index = int(input('PICK A MOVE: ')) - 1
+            selected_move, move_type = self.moves[index]
+            print(f'{self.name} used {selected_move}!')
             time.sleep(1)
-            
+
             effectiveness = calculate_effectiveness(move_type, pokemon2.types)
             damage = self.attack * effectiveness
 
@@ -125,18 +118,8 @@ class pokemon :
             elif effectiveness == 0.5:
                 print("\nIT'S NOT VERY EFFECTIVE...")
 
-            print(f"pokemon2's health: {pokemon2.healthBar}")
             pokemon2.healthBar -= damage
-            pokemon2.health = '=' * int(pokemon2.healthBar + 0.1 * pokemon2.defense)
-            
-
-            print(f'\n{string_1_attack}')
-
-            
-                
-
-            for j in range(int(pokemon2.healthBar+.1 * pokemon2.defense)):
-                pokemon2.health += '='
+            pokemon2.health = '=' * max(0, int(pokemon2.healthBar + 0.1 * pokemon2.defense))
 
             if pokemon2.healthBar <= 0:
                 print(f"\n... {pokemon2.name} fainted")
@@ -152,42 +135,36 @@ class pokemon :
 
             time.sleep(0.5)
 
-            print(f"{self.name} health: {self.healthBar}")
-            
-
-            
-    
             print(f'GO {pokemon2.name}!')
-            for i, x in enumerate(pokemon2.moves):
-                print(f'{i + 1}.', x)
-            index = int(input('PICK A MOVE'))
-            print(f'{pokemon2.name} used {pokemon2.moves[index - 1]}!')
+            for i, (move_name, move_type) in enumerate(pokemon2.moves):
+                print(f'{i + 1}. {move_name}')
+            index = int(input('PICK A MOVE: ')) - 1
+            selected_move, move_type = pokemon2.moves[index]
+            print(f'{pokemon2.name} used {selected_move}!')
             time.sleep(1)
-            
 
-            self.healthBar -= pokemon2.attack
-            self.health = ''
+            effectiveness = calculate_effectiveness(move_type, self.types)
+            damage = pokemon2.attack * effectiveness
 
-            print(f'\n{string_2_attack}')
+            if effectiveness == 2:
+                print("\nIT'S SUPER EFFECTIVE!")
+            elif effectiveness == 0.5:
+                print("\nIT'S NOT VERY EFFECTIVE...")
 
-            for j in range(int(self.healthBar+.1 * self.defense)):
-                self.health += '='
+            self.healthBar -= damage
+            self.health = '=' * max(0, int(self.healthBar + 0.1 * self.defense))
 
-            
             if self.healthBar <= 0:
                 print(f"\n... {self.name} fainted")
                 print(f"\n {pokemon2.name} has won!")
                 break
 
 
-
-
 if __name__ == "__main__":
+   
     
-    Charmander = pokemon('CHARMANDER', 'Fire', [('SCRATCH', 'Normal'), ('EMBER', 'Fire'), ('FLAMETHROWER', 'Fire'), ('TACKLE', 'Normal')], {'ATTACK': 4, 'DEFENSE': 2}, charmanderArt)
-    Squirtle = pokemon('SQUIRTLE', 'Water', [('TACKLE', 'Normal'), ('SURF', 'Water'), ('BUBBLE BLAST', 'Water'), ('WATERGUN', "Water")], {'ATTACK': 3, 'DEFENSE': 3}, squirtleArt)
-    # Bulbasaur = pokemon('BULBASAUR', 'Grass',['VINE WHIP', 'RAZOR LEAF', 'TACKLE', 'LEECH SEED'], {'ATTACK': 2, 'DEFENSE': 4}, bulbasaurArt)
+
+    Charmander = Pokemon('CHARMANDER', 'Fire', [('SCRATCH', 'Normal'), ('EMBER', 'Fire'), ('FLAMETHROWER', 'Fire'), ('TACKLE', 'Normal')], {'ATTACK': 4, 'DEFENSE': 2}, charmanderArt)
+    Squirtle = Pokemon('SQUIRTLE', 'Water', [('TACKLE', 'Normal'), ('SURF', 'Water'), ('BUBBLE BLAST', 'Water'), ('WATERGUN', 'Water')], {'ATTACK': 3, 'DEFENSE': 3}, squirtleArt)
 
     Charmander.fight(Squirtle)
-
-    
